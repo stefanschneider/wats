ce62aaa94ad8ccfa8a7c0f1055a8c82f1e684be5
John_Shahid <jvshahid@pivotal.io>
2015-05-11 15:12:57 -0400

    WIP

--- assets/nora/Nora/App_Start/WebApiConfig.cs
+++ assets/nora/Nora/App_Start/WebApiConfig.cs
@@ -7,7 +7,6 @@ namespace Nora
     {
         public static void Register(HttpConfiguration config)
         {
-            Directory.CreateDirectory(@"tmp\lifecycle\log\IIS\W3SVC1");
             // Web API configuration and services
 
             // Web API routes
